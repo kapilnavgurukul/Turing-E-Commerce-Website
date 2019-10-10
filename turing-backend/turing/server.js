1efdcd5097c46589349389ab2416afd38674b52d
kapil chouhan <45585049+kapilnavgurukul@users.noreply.github.com>
2019-10-10 09:51:41 +0530

    Update server.js

--- turing-backend/turing/server.js
+++ turing-backend/turing/server.js
@@ -7,6 +7,7 @@ const app=express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}));
 const port=parseInt(process.env.PORT)
+const secret_key=process.env.secret_key
 console.log(typeof(port))
 const knex=require("knex")({
     client: 'mysql',
@@ -20,31 +21,31 @@ const knex=require("knex")({
 
 
 app.use('/department',department=express.Router())
-require('./routes/department')(department,knex)
+require('./routes/department')(department,knex,secret_key)
 
 app.use("/categories",category=express.Router())
-require("./routes/category")(category,knex)
+require("./routes/category")(category,knex,secret_key)
 
 app.use("/attributes",attributes=express.Router())
-require("./routes/attributes")(attributes,knex)
+require("./routes/attributes")(attributes,knex,secret_key)
 
 app.use("/products",product=express.Router())
-require("./routes/product")(product,knex,jwt)
+require("./routes/product")(product,knex,jwt,secret_key)
 
 app.use("/customer",customer=express.Router())
-require("./routes/customer")(customer,knex,jwt)
+require("./routes/customer")(customer,knex,jwt,secret_key)
 
 app.use("/orders",orders=express.Router())
-require("./routes/orders")(orders,knex,jwt)
+require("./routes/orders")(orders,knex,jwt,secret_key)
 
 app.use("/shoppingcart",shoppingcart=express.Router())
-require("./routes/shoppingcart")(shoppingcart,knex,jwt)
+require("./routes/shoppingcart")(shoppingcart,knex,jwt,secret_key)
 
 app.use("/tax",tax=express.Router())
-require('./routes/tax')(tax,knex,jwt)
+require('./routes/tax')(tax,knex,jwt,secret_key)
 
 app.use('/shipping',shipping=express.Router())
-require("./routes/shipping")(shipping,knex,jwt)
+require("./routes/shipping")(shipping,knex,jwt,secret_key)
 
 app.listen(port,()=>{
     console.log(`server is listning on port ${port}`)
